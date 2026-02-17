dd872d9b81631c4e246a1eb6c43267953a3000db
Lalatiana RANDRIAMIHAJA <lalatiana.randriamihaja@etu.u-bordeaux.fr>
2026-02-17 16:58:06 +0100

    F2: clean up configManager

--- checkers/src/main/java/fr/ubordeaux/pdp/App.java
+++ checkers/src/main/java/fr/ubordeaux/pdp/App.java
@@ -11,11 +11,9 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-
-
-
 /**
- * Main class for the Checkers game. Handles command line arguments and initializes the game modes.
+ * Main class for the Checkers game. Handles command line arguments and
+ * initializes the game modes.
  *
  * @version 1.0
  */
@@ -37,7 +35,8 @@ public class App {
   private static boolean debug = false;
 
   /**
-   * Entry point of the application. Delegates logic to run() and handles exit codes.
+   * Entry point of the application. Delegates logic to run() and handles exit
+   * codes.
    *
    * @param args command line arguments
    */
@@ -59,16 +58,19 @@ public class App {
   }
 
   /**
-   * Parses arguments and sets global flags. This method is separated for unit testing purposes to
+   * Parses arguments and sets global flags. This method is separated for unit
+   * testing purposes to
    * avoid System.exit().
    *
-   * @param args command line arguments: -h/--help, -V/--version, -v/--verbose, -d/--debug.
-   * @return EXIT_SUCCESS to continue, EXIT_INFO to stop (info displayed), EXIT_ERROR for error.
+   * @param args command line arguments: -h/--help, -V/--version, -v/--verbose,
+   *             -d/--debug.
+   * @return EXIT_SUCCESS to continue, EXIT_INFO to stop (info displayed),
+   *         EXIT_ERROR for error.
    */
   public static int run(String[] args) {
     ConfigManager configManager = new ConfigManager();
     configManager.load();
-    verbose = Boolean.parseBoolean(configManager.getProperty("verbose", "false"));
+    verbose = configManager.isVerbose();
     // Options definition
     Options options = new Options();
     options.addOption("h", "help", false, "display help");
@@ -95,7 +97,6 @@ public class App {
 
       if (cmd.hasOption("v")) {
         verbose = true;
-        System.out.println("Verbose mode enabled.");
       }
 
       if (cmd.hasOption("d")) {
--- checkers/src/main/java/fr/ubordeaux/pdp/model/Utils.java
+++ checkers/src/main/java/fr/ubordeaux/pdp/model/Utils.java
@@ -6,19 +6,18 @@ import fr.ubordeaux.pdp.controller.*;
 
 public class Utils {
 
-	public static final int DEFAULT_BOARD_SIZE = 8;
-	public static final int DEFAULT_TIME = 30;
+  public static final int DEFAULT_BOARD_SIZE = 8;
+  public static final boolean DEFAULT_VERBOSE = false;
+  public static final boolean DEFAULT_BLITZ = false;
+  public static final int DEFAULT_TIME = 30;
 
   public static final Map<String, Command> COMMANDS_MAP = createCommandsMap();
 
-	
-  private static Map<String, Command> createCommandsMap(){
+  private static Map<String, Command> createCommandsMap() {
     return Map.of(
-      "new", new NewCommand(null, null),
-			"help", new HelpCommand(null),
-      "quit", new QuitCommand()
-    );
+        "new", new NewCommand(null, null),
+        "help", new HelpCommand(null),
+        "quit", new QuitCommand());
   }
-
 
 }
